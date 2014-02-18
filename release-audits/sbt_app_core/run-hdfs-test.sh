d4700ede391dfaf3aeecb78f52fe1fd2496b82ba
root <root@ip-10-4-186-145.ec2.internal>
2014-02-18 02:45:23 +0000

    Fix HdfsApp by concat-ing Services. Also update script

--- release-audits/sbt_app_core/run-hdfs-test.sh
+++ release-audits/sbt_app_core/run-hdfs-test.sh
@@ -5,5 +5,7 @@ FWDIR="$(cd `dirname $0`; pwd)"
 ~/ephemeral-hdfs/bin/hadoop fs -rm /input.txt
 ~/ephemeral-hdfs/bin/hadoop fs -copyFromLocal $FWDIR/input.txt / 
 
+pushd $FWDIR
 ./sbt/sbt clean assembly
 java -cp /root/spark-utils/release-audits/sbt_app_core/target/scala-2.10/simple-project-assembly-1.0.jar main.scala.SparkHdfsApp `cat /root/spark-ec2/masters`
+popd
